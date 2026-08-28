a63c54849da690921d0a18035a093cf030434081
JongPyoShin <48977434+JongPyoShin@users.noreply.github.com>
2026-08-28 13:48:31 +0900

    fix(space): install fail-closed full audit guard

--- nice-gui-1-1-7/portal_app/__init__.py
+++ nice-gui-1-1-7/portal_app/__init__.py
@@ -4,7 +4,9 @@ from . import space_routes as _space_routes  # noqa: F401
 from .space_direction_clarity import install_direction_clarity
 from .space_logic_reasoning import install_logic_reasoning
 from .space_quality_guard import install_quality_guard
+from .space_full_audit_guard import install_full_audit_guard
 
 install_direction_clarity()
 install_logic_reasoning()
 install_quality_guard()
+install_full_audit_guard()

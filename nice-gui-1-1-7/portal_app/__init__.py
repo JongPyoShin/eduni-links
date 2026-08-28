252290566b77be575512a2a7f2039d31971d7210
JongPyoShin <48977434+JongPyoShin@users.noreply.github.com>
2026-08-28 13:10:39 +0900

    feat(space): install logic reasoning extension

--- nice-gui-1-1-7/portal_app/__init__.py
+++ nice-gui-1-1-7/portal_app/__init__.py
@@ -2,5 +2,7 @@
 
 from . import space_routes as _space_routes  # noqa: F401
 from .space_direction_clarity import install_direction_clarity
+from .space_logic_reasoning import install_logic_reasoning
 
 install_direction_clarity()
+install_logic_reasoning()

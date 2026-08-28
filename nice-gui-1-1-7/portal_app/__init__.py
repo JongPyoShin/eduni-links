d5630dd5d9629b5167b83f925d59fd71b0644f3d
JongPyoShin <48977434+JongPyoShin@users.noreply.github.com>
2026-08-28 13:14:49 +0900

    feat(space): install spatial quality guard

--- nice-gui-1-1-7/portal_app/__init__.py
+++ nice-gui-1-1-7/portal_app/__init__.py
@@ -3,6 +3,8 @@
 from . import space_routes as _space_routes  # noqa: F401
 from .space_direction_clarity import install_direction_clarity
 from .space_logic_reasoning import install_logic_reasoning
+from .space_quality_guard import install_quality_guard
 
 install_direction_clarity()
 install_logic_reasoning()
+install_quality_guard()
